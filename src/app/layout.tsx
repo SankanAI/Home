b386664998dcd9669e946b92e290382aa1273c5c
shashankyadahalli <shashankyadahalli@gmail.com>
2024-12-09 11:36:48 +0000

    Launch Product

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,15 @@
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import AppAppBar from "./UI/AppAppBar"; // Adjust path as needed
 import Footer from "./UI/Footer";
-
-export const dm_sans_init = DM_Sans({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-dm-sans',
-  weight: ["300", "400", "500", "700"]
-});
+import './globals.css'; // Include your global CSS
 
 export const metadata: Metadata = {
   title: "Sankan AI",
   description: "Home Page",
 };
+
+
 
 export default function RootLayout({
   children,
@@ -23,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${dm_sans_init.variable}`}>
+      <head>
+      <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
       <AppAppBar />
         <main className="mt-24 container mx-auto px-4">
           {children}
